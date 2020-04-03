e627a1413b30b5ca3b70a3218822a221beb0e8af
MattiaFurlan-ts <63098758+MattiaFurlan-ts@users.noreply.github.com>
2020-04-03 16:23:40 +0200

    Update esercizio-C-2020-04-04.c

--- src/esercizio-C-2020-04-04.c
+++ src/esercizio-C-2020-04-04.c
@@ -1,1 +1,21 @@
-//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    char ch;
+    int current = 0, total = 0, rows = 0;
+    while ((ch = getchar ()) != EOF)
+    {
+            if (ch == '\n')
+            {
+                rows++;
+                total += current;
+                printf ("Lunghezza riga: %d, media parziale: %.1f\n", current, (float) total / rows);
+                current = 0;
+            }
+            else
+                current++;
+    }
+    return 0;
+}
